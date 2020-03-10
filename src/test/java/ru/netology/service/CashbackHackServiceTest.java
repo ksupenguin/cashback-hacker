997f3e541cd556b60c6444ba1e963a243604dcd7
Ksenia Glushkova <ksenia.glushkova96@gmail.com>
2020-03-10 23:20:46 +0300

    Added the first version of service, autotests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+
+    private final CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void checkZeroAmount() {
+        Assertions.assertEquals(1000, cashbackHackService.remain(0));
+    }
+
+    @Test
+    public void checkThousandAmount() {
+        Assertions.assertEquals(0, cashbackHackService.remain(1000));
+    }
+
+    @Test
+    public void checkHalfAmount() {
+        Assertions.assertEquals(500, cashbackHackService.remain(500));
+    }
+
+    @Test
+    public void checkAlmostThousandAmount() {
+        Assertions.assertEquals(999, cashbackHackService.remain(1));
+    }
+}
