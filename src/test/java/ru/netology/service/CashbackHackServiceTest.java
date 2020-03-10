52908eb7c8e1060b1ad966e1d0ff4763efc6c950
Ksenia Glushkova <ksenia.glushkova96@gmail.com>
2020-03-11 00:10:26 +0300

    Added parametrized tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CashbackHackServiceTest {
 
@@ -17,18 +19,9 @@ public class CashbackHackServiceTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(0));
     }
 
-    @Test
-    public void checkThousandAmount() {
-        Assertions.assertEquals(0, cashbackHackService.remain(1000));
-    }
-
-    @Test
-    public void checkHalfAmount() {
-        Assertions.assertEquals(500, cashbackHackService.remain(500));
-    }
-
-    @Test
-    public void checkAlmostThousandAmount() {
-        Assertions.assertEquals(999, cashbackHackService.remain(1));
+    @ParameterizedTest
+    @CsvFileSource(resources = "/params.csv", numLinesToSkip = 1)
+    public void checkAmount(int expected, int amount) {
+        Assertions.assertEquals(expected, cashbackHackService.remain(amount));
     }
 }
